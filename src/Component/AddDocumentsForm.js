966e475d2942218f63df1a19f0a6960058474f09
yvanvalaire <yvanvalaire23@gmail.com>
2024-07-12 23:28:02 +0100

    add function

--- src/Component/AddDocumentsForm.js
+++ src/Component/AddDocumentsForm.js
@@ -7,10 +7,17 @@
 
 
 import '../Css/AddDocumentsForm.css';
-export default function AddDocumentsForm(){
-    return(
-        <div className='App-adddocumentsform'>
-            Importer des document a ajouter a vos formation
+import img from '../Images/memberlist.png';
+
+export default function AddDocumentsForm() {
+    return (
+        <div className='App-addmembreform'>
+            <div className='ctn-addmembre'>
+                <img src={img} />
+                <p>Ajouter et gerer vos documents de vos formation</p>
+                <p>Ajouter des  Documents</p>
+                <p>Importer des Documents</p>
+            </div>
         </div>
     );
 };
